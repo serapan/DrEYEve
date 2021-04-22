d1bfc35ff36d85bc5af6af3b1c8095c2b0eb61ef
serapan <serapan150597@gmail.com>
2021-04-22 19:42:51 +0300

    8080

--- vue-app/src/config/index.ts
+++ vue-app/src/config/index.ts
@@ -1,7 +1,7 @@
 export class Config {
     public readonly ARTIFICIAL_NETWORK_DELAY: number = 0;
     public readonly HTTP_CLIENT_IMPL: string = 'axios';
-    public readonly BACKEND_API_URL: string = 'http://localhost:6969/api';
+    public readonly BACKEND_API_URL: string = 'http://localhost:8080/api';
 }
 
 const config: Config = new Config();
